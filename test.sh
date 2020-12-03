6d255789aeb809a679538141da3d68f78fdcb370
louiscklaw <louiscklaw@gmail.com>
2020-12-04 00:16:32 +0800

    update and test,

--- test.sh
+++ test.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-rm -rf /home/logic/_workspace/LynkedKK/QA_test_mac/github-actions-ios-emulator-tryout/.pytest_cache
+rm -rf github-actions-ios-emulator-tryout/.pytest_cache
 
-cd github-actions-ios-emulator-tryout
-  pipenv run python  -m pytest --html=output/ios14_test.html .
-cd ..
+pipenv run python -m pytest --html=output/ios13_test.h
+tml test_ios13.py

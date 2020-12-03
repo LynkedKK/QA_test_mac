5fa28859625d3439775b20d2ec1628cda6196327
louiscklaw <louiscklaw@gmail.com>
2020-12-04 01:11:26 +0800

    update and test,

--- test_upload.sh
+++ test_upload.sh
@@ -1,12 +1,12 @@
-git config --global user.email "louiscklaw@gmail.com"
-git config --global user.name "louiscklaw"
-git clone https://louiscklaw:$LOUISCKLAW_GITHUB_TOKEN@github.com/LynkedKK/QA_test_result.git --branch=master QA_test_result
+# git config --global user.email "louiscklaw@gmail.com"
+# git config --global user.name "louiscklaw"
+# git clone https://louiscklaw:$LOUISCKLAW_GITHUB_TOKEN@github.com/LynkedKK/QA_test_result.git --branch=master QA_test_result
 
-cp -r output_ios14  QA_test_result
+cp -r github-actions-ios-emulator-tryout/output_ios14/  QA_test_result/ios14
 
 ls -l QA_test_result
 
 cd QA_test_result
   git add .
-  git commit -m"update routine result,"
+  git commit -m"update routine result from ios14,"
   git push

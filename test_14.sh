9497fe8a77f025bad9b8ea6d03048cdfb03b627d
louiscklaw <louiscklaw@gmail.com>
2020-12-04 00:39:24 +0800

    update and test,

--- test_14.sh
+++ test_14.sh
@@ -4,6 +4,8 @@ set -ex
 
 rm -rf github-actions-ios-emulator-tryout/.pytest_cache
 
+git pull
+
 cd github-actions-ios-emulator-tryout
   pipenv run python -m pytest --html=output_ios14/index.html test_ios14.py
 cd -
